a35b47bd4ecdf6baf6b05063cd739cacf9332eb2
andubiel <andubiel@cisco.com>
2018-12-06 20:12:13 -0500

    Update hands_on_exercise.py

--- hands_on_exercise.py
+++ hands_on_exercise.py
@@ -13,9 +13,6 @@ pi = math.pi
 i = random.randint(0, 100)
 
 
-# TODO: Write a conditional that prints the color of the picked fruit
-picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
-
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
@@ -25,6 +22,4 @@ picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
 
 print("12 x 96 =",)
 
-print("48 x 17 =",)
 
-print("196523 x 87323 =",)
